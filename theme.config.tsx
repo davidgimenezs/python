72764926088be6d23826678c218d04d125b720b8
David Giménez <114882935+davidgimenezs@users.noreply.github.com>
2025-02-05 00:32:57 -0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Python</span>,
   project: {
-    link: 'https://github.com/davidgimenezs/python/tree/main',
+    link: 'https://github.com/davidgimenezs/python',
   },
   chat: {
     link: 'https://discord.gg/d5UVYtqv',
   },
-  docsRepositoryBase: 'https://github.com/davidgimenezs/python/tree/main',
+  docsRepositoryBase: 'https://github.com/davidgimenezs/python',
   footer: {
     text: 'De estudiantes, para estudiantes.',
   },
